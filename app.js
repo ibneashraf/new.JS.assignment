9ef13547ad75e7af9be6d6151afe540c0e0a970a
Ibneashraf <ibneashrafmadani@gmail.com>
2023-09-28 12:33:30 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+//Task 1
+var myName = "My name is ibne Asraf"
+alert(myName)
+
+//Task 2
+var a ="Please enter a valid passward"
+alert(a)
+
+//Task 3
+var b =" Welcome to JS Land... \n Happy coding!"
+alert(b)
+
+//Task 4
+var c = "Welcom to JS Land"
+var d = "Prevent this page from creating additional dialogs"
+alert(c)
+alert(d)
+
+//Task 5
+var e = "Hello I can run JS through my web browsers's console"
+console.log(e)
